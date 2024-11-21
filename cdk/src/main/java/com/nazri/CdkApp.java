932142d0aa236878689974f92cdbff84ed3f3306
649000 <649000@users.noreply.github.com>
2024-11-21 10:21:25 +0800

    CDK implementation

--- cdk/src/main/java/com/nazri/CdkApp.java
+++ cdk/src/main/java/com/nazri/CdkApp.java
@@ -1,19 +1,47 @@
 package com.nazri;
 
+import com.nazri.config.StackConfig;
+import com.nazri.stack.APIGatewayStack;
+import com.nazri.stack.DynamoDBStack;
+import com.nazri.stack.LambdaStack;
+import com.nazri.util.Constant;
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.StackProps;
 
 public class CdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new CdkStack(app, "CdkStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        // cdk synth --context env=dev OR --context env=sit OR --context env=uat
+        String environment = (String) app.getNode().tryGetContext("env");
+        StackConfig stackConfig = getStackConfig(environment);
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
+        DynamoDBStack dynamoDBStack = new DynamoDBStack(app, "currencycoinverter-dynamodb-stack", stackConfig.getStackProps()
+                .stackName("currencycoinverter-dynamodb-stack")
+                .description("DynamoDB Stack for Currency Coinverter")
+                .build(),
+                stackConfig);
+
+        LambdaStack apiLambdaStack = new LambdaStack(app, "currencycoinverter-api-lambda-stack", stackConfig.getStackProps()
+                .stackName("currencycoinverter-api-lambda-stack")
+                .description("Quarkus API Stack for Currency Coinverter")
+                .build(),
+                stackConfig,
+                dynamoDBStack.getUserTable()
+        );
+
+        new APIGatewayStack(app, "currencycoinverter-api-stack", stackConfig.getStackProps()
+                .stackName("currencycoinverter-api-stack")
+                .description("HTTP API Gateway Stack for Currency Coinverter")
+                .build(),
+                stackConfig,
+                apiLambdaStack.getAPIFunction());
+//        new CdkStack(app, "CdkStack", StackProps.builder()
+        // If you don't specify 'env', this stack will be environment-agnostic.
+        // Account/Region-dependent features and context lookups will not work,
+        // but a single synthesized template can be deployed anywhere.
+
+        // Uncomment the next block to specialize this stack for the AWS Account
+        // and Region that are implied by the current CLI configuration.
                 /*
                 .env(Environment.builder()
                         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
@@ -21,8 +49,8 @@ public class CdkApp {
                         .build())
                 */
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
+        // Uncomment the next block if you know exactly what Account and Region you
+        // want to deploy the stack to.
                 /*
                 .env(Environment.builder()
                         .account("123456789012")
@@ -30,10 +58,31 @@ public class CdkApp {
                         .build())
                 */
 
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+//                .build());
 
         app.synth();
+    }
+
+    public static StackConfig getStackConfig(String environment) {
+        StackConfig stackConfig;
+        switch (environment) {
+            case Constant.SIT:
+                stackConfig = new StackConfig.Builder()
+                        .withEnvironment(Constant.SIT)
+                        .build();
+                break;
+            case Constant.UAT:
+                stackConfig = new StackConfig.Builder()
+                        .withEnvironment(Constant.UAT)
+                        .build();
+                break;
+            default:
+                stackConfig = new StackConfig.Builder()
+                        .withEnvironment(Constant.DEV)
+                        .build();
+        }
+        return stackConfig;
     }
 }
 
