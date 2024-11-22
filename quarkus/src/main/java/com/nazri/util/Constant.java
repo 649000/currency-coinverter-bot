c964fca1a06136afb9cea0ecc9b3bebcd2587668
649000 <649000@users.noreply.github.com>
2024-11-23 01:34:16 +0800

    Added more constants to Quarkus app

--- quarkus/src/main/java/com/nazri/util/Constant.java
+++ quarkus/src/main/java/com/nazri/util/Constant.java
@@ -3,5 +3,9 @@ package com.nazri.util;
 public class Constant {
 
     public static final String USER_TABLE = "currencycoinverter-user";
-    public static final int CURRENCY_OUTPUT_LIMIT=3;
+    public static final int CURRENCY_OUTPUT_LIMIT = 3;
+
+    public static final String MARKDOWN = "Markdown";
+    public static final String MARKDOWNV2 = "MarkdownV2";
+    public static final String HTML = "HTML";
 }
