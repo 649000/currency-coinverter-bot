1ca3c06242750b10f65ae685b62f54e1ed8f783b
649000 <649000@users.noreply.github.com>
2024-11-23 01:36:47 +0800

    Removed unneeded files

--- quarkus/src/main/java/com/nazri/service/WebhookService.java
+++
@@ -1,9 +0,0 @@
-package com.nazri.service;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import org.jboss.logging.Logger;
-
-@ApplicationScoped
-public class WebhookService {
-    private static final Logger log = Logger.getLogger(WebhookService.class);
-}
