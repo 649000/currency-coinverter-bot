e278589339f5a6f52de43b50b15860ffa2af7abe
Nazri <649000@users.noreply.github.com>
2025-08-22 12:17:27 +0800

    refactor: update StartCommand to use MessageService for welcome message

--- quarkus/src/main/java/com/nazri/command/StartCommand.java
+++ quarkus/src/main/java/com/nazri/command/StartCommand.java
@@ -1,11 +1,11 @@
 package com.nazri.command;
 
+import com.nazri.service.MessageService;
 import com.nazri.service.TelegramBot;
+import com.nazri.service.TelegramResponse;
 import com.nazri.service.UserService;
-import com.nazri.util.Constant;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -17,6 +17,9 @@ public class StartCommand implements Command {
 
     @Inject
     UserService userService;
+
+    @Inject
+    MessageService messageService;
 
     @Override
     public String getName() {
