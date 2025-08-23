b63064639169c1c4d49a56190f24367d94ae5352
Nazri <649000@users.noreply.github.com>
2025-08-24 01:01:24 +0800

    docs: add javadocs to TagUtil class and method

--- cdk/src/main/java/com/nazri/util/TagUtil.java
+++ cdk/src/main/java/com/nazri/util/TagUtil.java
@@ -4,8 +4,22 @@ import com.nazri.config.StackConfig;
 import software.amazon.awscdk.Tags;
 import software.constructs.IConstruct;
 
+/**
+ * Utility class for applying standard tags to AWS resources.
+ * 
+ * This class provides helper methods to consistently apply project
+ * and environment tags to CDK constructs across all stacks.
+ */
 public class TagUtil {
 
+    /**
+     * Applies standard tags to the specified construct.
+     * 
+     * Adds project and environment tags based on the stack configuration.
+     * 
+     * @param iConstruct the CDK construct to tag
+     * @param stackConfig the stack configuration containing tag values
+     */
     public static void addTags(IConstruct iConstruct, StackConfig stackConfig) {
         Tags.of(iConstruct).add(Constant.PROJECT, Constant.CURRENCYCOINVERTER);
         Tags.of(iConstruct).add(Constant.ENVIRONMENT, stackConfig.getTags().get(Constant.ENVIRONMENT));
